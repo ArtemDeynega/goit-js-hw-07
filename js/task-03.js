5b71dfaee71a06c449ed598c6f9b2e9495fe87ad
Artem Deynega <artemdeynega892@gmail.com>
2021-06-10 16:41:18 +0300

    Сделал 3е задание без createElement и append

--- js/task-03.js
+++ js/task-03.js
@@ -1,17 +1,14 @@
 const images = [
   {
-    url:
-      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    url:
-      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url:
-      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
 ];
@@ -22,12 +19,22 @@ listEl.style.justifyContent = 'space-between';
 listEl.style.listStyle = 'none';
 listEl.style.alignItems = 'center';
 
-const createListItem = images.map(image => {
-  const itemEl = document.createElement('li');
-  itemEl.insertAdjacentHTML(
-    'beforeend',
-    `<img src='${image.url}' alt='${image.alt}' width='300px'>`,
-  );
-  return itemEl;
-});
-listEl.append(...createListItem);
+//Старый вариант
+
+// const createListItem = images.map(image => {
+//   const itemEl = document.createElement('li');
+//   itemEl.insertAdjacentHTML(
+//     'beforeend',
+//     `<img src='${image.url}' alt='${image.alt}' width='300px'>`,
+//   );
+//   return itemEl;
+// });
+// listEl.append(...createListItem);
+
+// Новый Вариант
+const createListItem = images
+  .map(({ url, alt }) => {
+    return `<img src='${url}' alt='${alt}' width='300px'>`;
+  })
+  .join('');
+listEl.insertAdjacentHTML('beforeend', createListItem);
